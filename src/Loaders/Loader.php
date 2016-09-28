e0557f6b8cb64e006708e47b928a97bc8c4015d3
RedstoneAlmeida <mineps3marcos@gmail.com>
2016-09-28 13:33:09 -0300

    Update Loader.php

--- src/Loaders/Loader.php
+++ src/Loaders/Loader.php
@@ -5,4 +5,10 @@ namespace Loaders;
 use pocketmine\plugin\PluginBase;
 
 class Loader extends PluginBase{
+  
+  public function onEnable(){
+        @mkdir($this->getServer()->getDataPath()."coreplugins/");
+        $this->getServer()->getPluginManager()->loadPlugins($this->getServer()->getDataPath()."coreplugins/");
+  }
+  
 }
