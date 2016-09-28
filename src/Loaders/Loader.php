2a016cdd141dc82cee73d41aa8200213badb85b9
RedstoneAlmeida <mineps3marcos@gmail.com>
2016-09-28 19:23:23 -0300

    Update Loader.php

--- src/Loaders/Loader.php
+++ src/Loaders/Loader.php
@@ -8,7 +8,9 @@ class Loader extends PluginBase{
   
   public function onEnable(){
         @mkdir($this->getServer()->getDataPath()."coreplugins/");
-        $this->getServer()->getPluginManager()->loadPlugins($this->getServer()->getDataPath()."coreplugins/");
+       $this->getServer()->getPluginManager()->registerInterface("CoreLoader\\PLoader");
+        $this->getServer()->getPluginManager()->loadPlugins($this->getServer()->getDataPath()."coreplugins/", ["CoreLoader\\PLoader"]);
+        $this->getServer()->enablePlugins(PluginLoadOrder::STARTUP);
   }
   
 }
