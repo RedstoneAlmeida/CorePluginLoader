19888785ffda4203466199dd985aa12abd314e03
RedstoneAlmeida <mineps3marcos@gmail.com>
2016-09-29 20:25:47 -0300

    Update API.php

--- src/NewAPI/API.php
+++ src/NewAPI/API.php
@@ -1,1 +1,18 @@
 <?php
+
+namespace NewAPI;
+
+use pocketmine\plugin\PluginBase;
+
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+
+use NewAPI\Core;
+
+interface NewAPI{
+  
+  public function cmdAPI(CommandSender $sender, Command $command, $label, Array $args);
+  
+  public function API(Core $core);
+  
+}
